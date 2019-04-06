9d27eda138dcb5ee75f434926f4e1e6ef6dd34ad
Caio Costa <caio.costacarmo@gmail.com>
2019-04-06 18:21:52 -0300

    Tela de pagamento

---
+++ app/src/main/java/com/example/caio/popsodadrink/PagamentoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.caio.popsodadrink
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PagamentoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagamento)
+    }
+}
