0a7692262709757a019fcfb4a2caa3670c72c092
Caio Costa <caio.costacarmo@gmail.com>
2019-04-07 21:27:03 -0300

    Layout 80% finalizado

---
+++ app/src/main/java/com/example/caio/popsodadrink/PerfilActivity.kt
@@ -0,0 +1,13 @@
+
+package com.example.caio.popsodadrink
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PerfilActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
